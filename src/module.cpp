542f89a37fe72903c2dcbb8ca1ff70d374830772
Hui Peng Hu <woohp135@gmail.com>
2020-11-12 13:14:51 -0800

    upgrade to pybind11 2.6.0, and use the setuptool helpers that it provides

--- src/module.cpp
+++ src/module.cpp
@@ -3,6 +3,9 @@
 #include <string>
 namespace py = pybind11;
 
+#define STRINGIFY(x) #x
+#define MACRO_STRINGIFY(x) STRINGIFY(x)
+
 
 PYBIND11_MODULE(unidecode, m)
 {
@@ -13,6 +16,6 @@ PYBIND11_MODULE(unidecode, m)
     m.def("unidecode", &unidecode, "Transliterate an Unicode object into an ASCII string.", "x"_a);
 
 #ifdef VERSION_INFO
-    m.attr("__version__") = VERSION_INFO;
+    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
 #endif
 }
